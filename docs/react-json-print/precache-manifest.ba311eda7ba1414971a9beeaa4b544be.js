29c529575d0e38717b2b66a2b67119cc95505029
tanmancan <contact@tkarimdesign.com>
2019-12-20 16:48:17 -0500

    Updated public path

---
+++ docs/react-json-print/precache-manifest.ba311eda7ba1414971a9beeaa4b544be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85fe024569e14321cbc87a9b4946bfdf",
+    "url": "/examples/react-json-print/index.html"
+  },
+  {
+    "revision": "e056c2f0c21005b76e27",
+    "url": "/examples/react-json-print/static/css/main.ff4a52a5.chunk.css"
+  },
+  {
+    "revision": "b2dce2a21f7d6548af7d",
+    "url": "/examples/react-json-print/static/js/2.3b647bac.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/examples/react-json-print/static/js/2.3b647bac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e056c2f0c21005b76e27",
+    "url": "/examples/react-json-print/static/js/main.c39ccce6.chunk.js"
+  },
+  {
+    "revision": "2b07b5fbe350f4cedabd",
+    "url": "/examples/react-json-print/static/js/runtime-main.70263319.js"
+  }
+]);
--- docs/react-json-print/service-worker.js
+++ docs/react-json-print/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.377d97df1bce5675b8db5ae8d1649a99.js"
+  "/examples/react-json-print/precache-manifest.ba311eda7ba1414971a9beeaa4b544be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/examples/react-json-print/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
