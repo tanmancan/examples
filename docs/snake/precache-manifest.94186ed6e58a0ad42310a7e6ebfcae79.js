8d1c3f75b5a943ca4d9a9d2f1b6a5c44496f0508
tanmancan <contact@tkarimdesign.com>
2020-09-11 21:06:14 -0400

    Added snake

---
+++ docs/snake/precache-manifest.94186ed6e58a0ad42310a7e6ebfcae79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9b60dbfa74a3413a6fc997eb4b161e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d60e327471ac54a47a1e",
+    "url": "/static/css/main.2237b7f0.chunk.css"
+  },
+  {
+    "revision": "15041486e2eedafb0ec3",
+    "url": "/static/js/2.5cc6967a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5cc6967a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d60e327471ac54a47a1e",
+    "url": "/static/js/main.424bdaca.chunk.js"
+  },
+  {
+    "revision": "d0c6a9763e2577b7f05c",
+    "url": "/static/js/runtime-main.76397671.js"
+  }
+]);
